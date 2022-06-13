bfca18875451b07765be6000c47322ec73846c67
Edson Daniel Navarro Brizuela <edsonnavarro0710@gmail.com>
2022-06-13 08:40:48 -0500

    Avance stories

--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_facebook/models/models.dart';
@@ -15,32 +16,58 @@ class Stories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200.0,
-      color: Colors.purple,
-      // child: ListView.builder(
-      //   padding: const EdgeInsets.symmetric(
-      //     vertical: 10.0,
-      //     horizontal: 8.0,
-      //   ),
-      //   scrollDirection: Axis.horizontal,
-      //   itemCount: 1 + stories.length,
-      //   itemBuilder: (BuildContext context, int index) {
-      //     if (index == 0) {
-      //       return Padding(
-      //         padding: const EdgeInsets.symmetric(horizontal: 4.0),
-      //         child: _StoryCard(
-      //           isAddStory: true,
-      //           currentUser: currentUser,
-      //         ),
-      //       );
-      //     }
-      //     final Story story = stories[index - 1];
-      //     return Padding(
-      //       padding: const EdgeInsets.symmetric(horizontal: 4.0),
-      //       child: _StoryCard(story: story),
-      //     );
-      //   },
-      // )
+        height: 200.0,
+        color: Colors.purple,
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 8.0,
+          ),
+          scrollDirection: Axis.horizontal,
+          itemCount: 1 + stories.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: _StoryCard(
+                  isAddStory: true,
+                  currentUser: currentUser,
+                ),
+              );
+            }
+            final Story story = stories[index - 1];
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: _StoryCard(story: story,),
+            );
+          },
+        ));
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  final bool isAddStory;
+  final User currentUser;
+  final Story story;
+
+  const _StoryCard({
+    Key? key,
+    this.isAddStory = false,
+    this.currentUser,
+    this.story,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CachedNetworkImage(
+          imageUrl: isAddStory ? currentUser.imageUrl : story.imageUrl,
+          height: double.infinity,
+          width: 110.0,
+          fit: BoxFit.cover,
+        ),
+      ],
     );
   }
 }
